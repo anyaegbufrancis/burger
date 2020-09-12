3e224956f406b6dd75b06a8ac4e5ef3adb70d164
Anyaegbu Francis <anyaegbufrancis@gmail.com>
2020-09-12 23:22:37 +0400

    config file update

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,6 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
   password: "root",
   database: "burgers_db"
@@ -15,13 +14,6 @@ if (process.env.JAWSDB_URL) {
 }
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+connection.connect()
 // Export connection for our ORM to use.
 module.exports = connection;
