a92b3e6dfad9726a5854fd3b56e15472b2516f28
Anyaegbu Francis <anyaegbufrancis@gmail.com>
2020-09-12 08:05:24 +0400

    db update

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name) VALUES ('Beef Burger');
+INSERT INTO burgers (burger_name) VALUES ('Elk Burger');
+INSERT INTO burgers (burger_name) VALUES ('Portobello Mushroom Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
+INSERT INTO burgers (burger_name) VALUES ('Wild Salmon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Burger');
+
